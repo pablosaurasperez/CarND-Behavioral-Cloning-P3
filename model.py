c264a706ab75863e7ad616997595e2829ac4749e
Pablo Sauras <carnd@ip-172-31-74-224.ec2.internal>
2017-07-12 20:25:45 +0000

    Apply LeNet

--- model.py
+++ model.py
@@ -41,20 +41,38 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 
-#Apply a simple regression network (no activation function needed)
+#Test LeNet
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 #Prepocess data. Normalize and mean center
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+
+#LeNet
+model.add(Convolution2D(6, 5, 5 , activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+
+model.add(Convolution2D(16, 5, 5 , activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+
 model.add(Flatten())
+
+model.add(Dense(120))
+model.add(Activation('relu'))
+
+model.add(Dense(84))
+model.add(Activation('relu'))
+
 model.add(Dense(1))
+#end LeNet
 
 #MSE (predicted-GT) because is regression network instead of classification (Cross_Entropy)
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 #Save the trained model so that I can download it to my local machine an see if it's able to go autonomous
 model.save('model.h5')
