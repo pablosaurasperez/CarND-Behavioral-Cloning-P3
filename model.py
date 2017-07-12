04bac3e49bf9c8fba10a8b15d1ef972f6f5593dc
Pablo Sauras <carnd@ip-172-31-74-224.ec2.internal>
2017-07-12 23:25:28 +0000

    Substitute LeNet by NVIDIA Net

--- model.py
+++ model.py
@@ -67,7 +67,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 
-#Test LeNet
+#Test NVIDIA Network
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
@@ -81,27 +81,31 @@ model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160, 320, 3)))
 #Normalize and mean center
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
-#LeNet
-model.add(Convolution2D(6, 5, 5 , activation='relu'))
-model.add(MaxPooling2D((2, 2)))
+#NVIDIA
 
-model.add(Convolution2D(16, 5, 5 , activation='relu'))
-model.add(MaxPooling2D((2, 2)))
+#5 convolutional layers
+#Stride 2x2
+model.add(Convolution2D(24, 5, 5 , subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36, 5, 5 , subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48, 5, 5 , subsample=(2,2), activation='relu'))
+#No stride
+model.add(Convolution2D(64, 3, 3 , activation='relu'))
+model.add(Convolution2D(64, 3, 3 , activation='relu'))
 
 model.add(Flatten())
 
-model.add(Dense(120))
-model.add(Activation('relu'))
+#3 fully connected layers
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 
-model.add(Dense(84))
-model.add(Activation('relu'))
-
+#Output layer
 model.add(Dense(1))
 #end LeNet
 
 #MSE (predicted-GT) because is regression network instead of classification (Cross_Entropy)
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=3)
 
 #Save the trained model so that I can download it to my local machine an see if it's able to go autonomous
 model.save('model.h5')
